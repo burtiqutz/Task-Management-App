1fa2be6a58e762821c8ec5e6cb9a5730520e5dd5
alex <alex_rock04@yahoo.com>
2025-03-09 19:15:26 +0200

    Fixed hashcode

--- src/main/java/dataModel/Employee.java
+++ src/main/java/dataModel/Employee.java
@@ -27,11 +27,7 @@ public class Employee {
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + idEmployee;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
+        return Integer.hashCode(idEmployee);
     }
 
     @Override
